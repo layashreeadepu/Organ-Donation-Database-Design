1271e817fbe25cfb1518cc225af8d2d8210858f5
Layashree Adepu <62901388+layashreeadepu@users.noreply.github.com>
2024-04-11 13:27:45 -0400

    Add files via upload

---
+++ Inser Queries/Organ List.sql
@@ -0,0 +1,8 @@
+insert into Organ_List (Organ_ID, Organ_Name, Required_Specialist) values (1, 'Vascularized allograft (VCA)', 'Microsurgery Specialist');
+insert into Organ_List (Organ_ID, Organ_Name, Required_Specialist) values (2, 'Heart', 'Cardiologist');
+insert into Organ_List (Organ_ID, Organ_Name, Required_Specialist) values (3, 'Intestine', 'Gastroenterologist');
+insert into Organ_List (Organ_ID, Organ_Name, Required_Specialist) values (4, 'Liver', 'Hepatologist');
+insert into Organ_List (Organ_ID, Organ_Name, Required_Specialist) values (5, 'Kidney', 'Nephrologist');
+insert into Organ_List (Organ_ID, Organ_Name, Required_Specialist) values (6, 'Lung', 'Pulmonologist');
+insert into Organ_List (Organ_ID, Organ_Name, Required_Specialist) values (7, 'Pancreas', 'Endocrinologist');
+insert into Organ_List (Organ_ID, Organ_Name, Required_Specialist) values (8, 'Eyes', 'Ophthalmologist');
---
+++ Inser Queries/Transplantation.sql
@@ -0,0 +1,120 @@
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values (1, 2, 1, 1, 2, '05/25/2026', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values (2, 2, 2, 2, 2, '01/23/2008', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values (3, 3, 3, 3, 1.5, '12/29/2018', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values (4, 4, 4, 4, 1.2, '01/27/2002', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values (5, 5, 5, 5, 4.6, '01/03/2021', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values (6, 6, 6, 6, 3.2, '02/20/2022', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values (7, 7, 7, 7, 1.5, '12/01/2003', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values (8, 8, 8, 8, 4.6, '01/13/2025', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values (9, 9, 9, 9, 3.2, '10/29/2027', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 10, 10, 10, 10, 3.2, '02/28/2019', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 11, 11, 11, 11, 2.5, '08/04/2022', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 12, 12, 12, 12, 1, '06/12/2024', 'Scheduled');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 13, 13, 13, 13, 2.5, '10/08/2024', 'Scheduled');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 14, 14, 14, 14, 3, '04/13/2000', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 15, 15, 15, 15, 4.3, '09/09/2024', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 16, 16, 16, 16, 3, '08/23/2024', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 17, 17, 17, 17, 1.2, '12/19/2007', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 18, 18, 18, 18, 2.3, '06/05/2001', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 19, 19, 19, 19, 3.5, '04/25/2025', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 20, 20, 20, 20, 4.3, '02/27/2028', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 21, 21, 21, 21, 2, '09/30/2025', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 22, 22, 22, 22, 1.2, '10/26/2003', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 23, 23, 23, 23, 3.5, '09/16/2011', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 24, 24, 24, 24, 3, '04/26/2001', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 25, 25, 25, 25, 1.2, '08/14/2026', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 26, 26, 26, 26, 4.6, '12/30/2004', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 27, 27, 27, 27, 3.7, '05/18/2026', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 28, 28, 28, 28, 2.5, '03/27/2000', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 29, 29, 29, 29, 2, '04/26/2015', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 30, 30, 30, 30, 2.5, '11/19/2007', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 31, 2, 31, 31, 1, '11/19/2024', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 32, 2, 32, 32, 5, '09/08/2024', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 33, 3, 33, 33, 4.6, '01/02/2025', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 34, 4, 34, 34, 4.3, '08/15/2026', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 35, 5, 35, 35, 2, '09/29/2026', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 36, 6, 36, 36, 4.6, '01/15/2026', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 37, 7, 37, 37, 4.1, '09/06/2020', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 38, 8, 38, 38, 4.6, '05/08/2012', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 39, 9, 39, 39, 2.3, '03/18/2025', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 40, 10, 40, 40, 2, '12/15/2005', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 41, 11, 41, 41, 3.5, '10/25/2026', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 42, 12, 42, 42, 2.5, '12/09/2012', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 43, 13, 43, 43, 4.3, '08/13/2024', 'Scheduled');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 44, 14, 44, 44, 3.2, '01/18/2003', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 45, 15, 45, 45, 3.3, '04/05/2010', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 46, 16, 46, 46, 3.2, '03/26/2007', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 47, 17, 47, 47, 5, '04/16/2024', 'Scheduled');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 48, 18, 48, 48, 4.3, '08/18/2024', 'Scheduled');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 49, 19, 49, 49, 1.2, '05/29/2024', 'Scheduled');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 50, 20, 50, 50, 4.3, '05/08/2025', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 51, 21, 51, 1, 2, '02/27/2016', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 52, 22, 52, 2, 3.7, '10/02/2024', 'Scheduled');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 53, 23, 53, 3, 1, '07/06/2024', 'Scheduled');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 54, 24, 54, 4, 1, '05/06/2024', 'Scheduled');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 55, 25, 55, 5, 2.3, '08/25/2022', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 56, 26, 56, 6, 3.7, '03/24/2000', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 57, 27, 57, 7, 3.7, '03/28/2025', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 58, 28, 58, 8, 3.3, '09/28/2027', 'Reschedule');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 59, 29, 59, 9, 2.3, '12/21/2016', 'Completed');
+insert into Transplantation (Person_ID, Hospital_ID, Donated_Organ_ID, Doctor_ID, Organ_Lifespan, Transplantation_Date, Transplantation_Status) 
+values ( 60, 30, 60, 10, 2.5, '04/23/2024', 'Scheduled');
